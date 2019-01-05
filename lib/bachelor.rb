deed5ece9a338f1f16ad3591894adbe787dc8eb5
Heloise Taillet <ht2394@columbia.edu>
2019-01-05 03:02:03 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -67,7 +67,7 @@ def get_average_age_for_season(data, season)
         end
       end
     end
-  divider= array.size+1
+  divider= array.size
   avg = number/divider
   if number%divider > 0.3
     avg += 1
